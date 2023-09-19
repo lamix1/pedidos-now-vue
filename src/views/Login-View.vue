ce29ddbb80e4fbdf5d30e2a32f15a385a04ca5e8
luizatorrens <luiza.torrens.2006@gmail.com>
2023-09-19 14:38:43 -0300

    Alteração no CSS da página de login.

--- src/views/Login-View.vue
+++ src/views/Login-View.vue
@@ -1,54 +1,59 @@
 <template width="100%" height="100%">
-        <div class="container">
+  <div class="td">
+    <div class="card w-25 position-absolute top-50 start-50 translate-middle" style="width: 20rem">
+      <div class="card-body">
+        <img src="../assets/PedidosNOW.png" class="card-img-top " />
 
-          <div class="card w-100" style="width: 20rem;">
-            <div class="card-body">
-            <img src="../assets/PedidosNOW.png" class="card-img-top" width="200px">
-
-            <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Senha</label>
-  <input type="password" class="form-control" id="exampleFormControlInput1" >
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-  <label class="form-check-label" for="flexCheckDefault">Lembrar de mim</label>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label"></label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label"></label>
+          <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="*****"/>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+          <label class="form-check-label" for="flexCheckDefault">Lembrar de mim</label>
+        </div>
+        <router-link to="/Home.vue" custom v-slot="{ navigate }">
+          <button
+            @click="navigate"
+            role="link"
+            type="button"
+            class="btn btn-primary btn-lg"
+          >
+            Login
+          </button>
+        </router-link>
+      </div>
+    </div>
   </div>
-  <router-link to="/Home.vue" custom v-slot="{ navigate }">
-  <button
-  @click="navigate"
-  role="link" 
-  type="button" 
-  class="btn btn-primary btn-lg"
-  style="
-  
-   "
-  >
-  Login
-</button>
-</router-link>
-</div>
-            </div>
-          </div>
 </template>
 <style>
-.container {
-    background-image: url('../assets/restaurante_updated.png');
-    height: 100vh;
-    background-size: cover;
-  }
+
+.td {
+  background-image: url('../assets/restaurante_updated.png');
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  position: relative;
+  margin: 0;
+}
 .card-body {
-  justify-content: center;
   border-radius: 10px;
   background-color: white;
-  width: 300px;
+  align-items: center;
 }
 .card-img-top {
-  margin-left: 50px;
   display: flex;
+  align-items: center;
   justify-content: center;
+  width: 300px;
 }
 </style>
